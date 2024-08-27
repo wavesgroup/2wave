02ff845dfd3646e076be21f9174659cc45099451
milancurcic <caomaco@gmail.com>
2024-08-27 13:45:14 -0400

    A few more basic tests

--- test_twowave.py
+++ test_twowave.py
@@ -1,11 +1,29 @@
 import numpy as np
-from twowave import angular_frequency, elevation
+from twowave import angular_frequency, elevation, gravity, WaveModulationModel
+import xarray as xr
+
+
+G0 = 9.8
 
 
 def test_angular_frequency():
-    assert angular_frequency(9.8, 1) == np.sqrt(9.8)
-    assert angular_frequency(9.8, 1, 1) == np.sqrt(9.8) + 1
+    assert angular_frequency(G0, 1) == np.sqrt(G0)
+    assert angular_frequency(G0, 1, 1) == np.sqrt(G0) + 1
 
 
 def test_elevation():
     assert elevation(0, 0, 1, 1, 1, wave_type="linear") == 1
+
+
+def test_gravity():
+    assert gravity(0, 0, 0, 1, G0, wave_type="linear") == G0
+    assert gravity(0, 0, 0, 1, G0, wave_type="stokes") == G0
+
+
+def test_wave_modulation_model():
+    m = WaveModulationModel(num_periods=1)
+    m.run()
+    ds = m.to_xarray()
+    assert type(ds) == xr.Dataset
+    assert np.all(np.isfinite(ds.wavenumber))
+    assert np.all(np.isfinite(ds.amplitude))
